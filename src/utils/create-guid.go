d7aa74f74e0210a5a72a05338d46f42387d73852
Oleg Panikarovskiy <oleg.pnk@gmail.com>
2020-04-19 01:12:40 +0300

    small imp

--- src/utils/create-guid.go
+++ src/utils/create-guid.go
@@ -2,10 +2,8 @@ package utils
 
 import (
 	"crypto/rand"
-	"encoding/hex"
 	"fmt"
 	"log"
-	"math"
 )
 
 // CreateGUID generates guid string
@@ -18,17 +16,4 @@ func CreateGUID() string {
 	}
 
 	return fmt.Sprintf("%x-%x-%x-%x-%x", buff[0:4], buff[4:6], buff[6:8], buff[8:10], buff[10:])
-}
-
-// RandomString gen random string
-func RandomString(size int) string {
-	buff := make([]byte, int(math.Round(float64(size)/2)))
-
-	_, err := rand.Read(buff)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	str := hex.EncodeToString(buff)
-	return str[:size]
 }
